25bbba59f4a7c0a4626ec65f59598c4f6ce49d54
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-05-10 17:11:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ class Config(object):
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "9972ace070bae89fbb76f073d56e41e5c458c302")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
@@ -18,32 +18,30 @@ class Config(object):
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
-This is a Permanent FileStore Bot. 
-Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+This Is A Permanent FileStore Bot. 
+Send Me Any Media Or File. I Can Work In Channel Too. Add Me To Channel With Edit Permission, I Will Add Save Uploaded File In Channel And Share A Shareable Link. 
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name ☞ [Infinity File Saver](https://t.me/{BOT_USERNAME})
 │
-├🔸 Language: [Python 3](https://www.python.org)
+├🔸 Language ☞ [Python 3](https://www.python.org)
 │
-├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
+├🔹 Library ☞ [Pyrogram](https://docs.pyrogram.org)
 │
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [DEAD](https://telegram.me/DRDIC)
  
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
+[Want To Talk With Me !](https://t.me/DRDIC)
 """
   HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+Hello, [{}](tg://user?id={})\n\nThis Is A Permanent **FileStore Bot**.
 
-How to Use Bot & it's Benefits??
+How to Use Bot & It's Benefits??
 
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+📢 Send Me Any File & It Will Be Uploaded In My Database & You Will Get The File Link.
 
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+⚠️ Benefits: If You Have A TeleGram Movie Channel Or Any Copyright Channel, Then Its Useful For Daily Usage, You Can Send Me Your File & I Will Send Permanent Link To You & Channel Will Be Safe From **CopyRight Infringement** Issue. I Support Channel Also You Can Check **About Bot**.
 """
